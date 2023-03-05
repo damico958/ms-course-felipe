05c8a210f7d37098b66088b0ac8e7ba243cd321a
Felipe D'Amico <damico.lipe@gmail.com>
2023-03-05 17:23:57 -0300

    Feature: LIPE-07 - Adding Netflix Hystrix on Payroll for Fallback behavior

--- hr-payroll/src/main/java/com/felipe/hrpayroll/controller/PaymentController.java
+++ hr-payroll/src/main/java/com/felipe/hrpayroll/controller/PaymentController.java
@@ -2,6 +2,7 @@ package com.felipe.hrpayroll.controller;
 
 import com.felipe.hrpayroll.entity.Payment;
 import com.felipe.hrpayroll.service.PaymentService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/payments")
 public class PaymentController {
+    private static final Double DAILY_INCOME = 1000.0;
 
     private final PaymentService paymentService;
 
@@ -18,9 +20,15 @@ public class PaymentController {
         this.paymentService = paymentService;
     }
 
+    @HystrixCommand(fallbackMethod = "getPaymentFallback")
     @GetMapping(value = "/{workerId}/days/{days}")
     public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days) {
         Payment payment = paymentService.getPayment(workerId, days);
+        return ResponseEntity.ok(payment);
+    }
+
+    public ResponseEntity<Payment> getPaymentFallback(Long workerId, Integer days) {
+        Payment payment = new Payment("Felipe", DAILY_INCOME, days);
         return ResponseEntity.ok(payment);
     }
 
