2f32635a0263acb189b968523e205efd4d2c8b6b
Felipe D'Amico <damico.lipe@gmail.com>
2023-03-19 11:12:34 -0300

    Feature: LIPE-17 - Adding Login, Token JWT and Zuul Resource Authorization Configuration

--- hr-oauth/src/main/java/com/felipe/hroauth/service/UserService.java
+++ hr-oauth/src/main/java/com/felipe/hroauth/service/UserService.java
@@ -4,10 +4,13 @@ import com.felipe.hroauth.client.UserFeignClient;
 import com.felipe.hroauth.entity.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
     Logger logger = LoggerFactory.getLogger(UserService.class);
 
     private final UserFeignClient userFeignClient;
@@ -23,6 +26,17 @@ public class UserService {
             throw new IllegalArgumentException("Email not found");
         }
         logger.info("Email found: " + email);
+        return user;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userFeignClient.findByEmail(username).getBody();
+        if (user == null) {
+            logger.error("Username not found: " + username);
+            throw new UsernameNotFoundException("Username not found");
+        }
+        logger.info("Username found: " + username);
         return user;
     }
 }
